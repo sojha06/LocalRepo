d4b34985a303e515955280495914fa7c6eabc173
Shivam Ojha <sojha06@gmail.com>
2025-06-19 16:54:46 +0530

    Added Comment

--- adapters/KafkaAdapter.js
+++ adapters/KafkaAdapter.js
@@ -1,17 +1,2 @@
 //Added Sample Change JS
-const car = {
-  make: "Toyota",
-  model: "Camry",
-  year: 2023,
-
-  // This is a method
-  displayInfo: function() {
-    console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
-  },
-
-  // Another method to update the year
-  updateYear: function(newYear) {
-    this.year = newYear;
-    console.log(`Car year updated to: ${this.year}`);
-  }
-};
+// Adapter Code
